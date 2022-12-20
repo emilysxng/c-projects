60fe37b6c4a004aeb2ce5c190a69fd0ef74c7c1c
Emily Song <song.emily2004@gmail.com>
2022-12-19 19:27:27 -0800

    rename folders

--- hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-int main(void)
-{
-    //asks user for name (input) and gets a string
-    string nameinput = get_string("what's your name?\n");
-    //prints hello, name with the string input
-    printf("hello, %s\n", nameinput);
-}
